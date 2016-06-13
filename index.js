b0abac6b2c7ab68765f74ca73c0a8a40fa256db4
Stefan Härtel <haertel.dev@gmail.com>
2016-06-13 11:23:09 +0200

    Adjust structure

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+// includes
 var express = require('express');
 var path = require('path');
 var app = express();
 
+// setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
-
+// initialization
 app.listen(3000, function () {
   console.log('EcoRealms backend is listening on port 3000!');
+});
+
+// routes
+app.get('/', function (req, res) {
+  res.render('index');
 });
