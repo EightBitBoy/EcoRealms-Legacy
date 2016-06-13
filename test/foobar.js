69edb8c3cb8f87e59027a969eda2eca4a0439a52
Stefan Härtel <haertel.dev@gmail.com>
2016-06-13 10:25:54 +0200

    Adjust test

--- test/foobar.js
+++ test/foobar.js
@@ -1,11 +1,11 @@
 var assert = require('chai').assert;
 
-describe('FooBar', function() {
-  describe('index', function () {
-    it('should work', function () {
+describe('Array', function() {
+  describe('indexOf()', function () {
+    it('should find value', function () {
       assert.equal(4, [1,2,3,4,5,6,7].indexOf(5));
     });
-    it('should fail', function () {
+    it('should not find value', function () {
       assert.equal(-1, [1,2,3,4,5,6,7].indexOf(10));
     });
   });
