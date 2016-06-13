b4f96b6f670149bea6bcffddce7ed165618c370a
Stefan Härtel <haertel.dev@gmail.com>
2016-06-13 10:11:03 +0200

    Adjust test, add chai dependency

--- test/foobar.js
+++ test/foobar.js
@@ -2,8 +2,10 @@ var assert = require('chai').assert;
 
 describe('FooBar', function() {
   describe('index', function () {
-    it('should work once and fail once', function () {
+    it('should work', function () {
       assert.equal(4, [1,2,3,4,5,6,7].indexOf(5));
+    });
+    it('should fail', function () {
       assert.equal(-1, [1,2,3,4,5,6,7].indexOf(10));
     });
   });
