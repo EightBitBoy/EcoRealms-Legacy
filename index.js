d2e2eb19df96697038d51d801d25106f4376764b
Stefan Härtel <haertel.dev@gmail.com>
2016-06-13 10:53:28 +0200

    Use jade templates

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.render('index');
 });
 
 app.listen(3000, function () {
