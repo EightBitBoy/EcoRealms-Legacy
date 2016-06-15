45007e3f0d6fa6ebd3677328d141e22c6c280240
Stefan Härtel <haertel.dev@gmail.com>
2016-06-15 17:25:54 +0200

    Use custom canvas

--- public/javascripts/playground.js
+++ public/javascripts/playground.js
@@ -3,12 +3,14 @@ console.log("Hello from playground.js!")
 var WIDTH = 600
 var HEIGHT = 450
 
+var VIEWPORT = document.getElementById("viewport");
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({canvas: VIEWPORT});
 renderer.setSize( WIDTH, HEIGHT );
-document.body.appendChild( renderer.domElement );
+//document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
