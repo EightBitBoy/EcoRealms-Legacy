749ea58d6956ced171acbeb95cbef930e8b2f3eb
Stefan Härtel <haertel.dev@gmail.com>
2016-06-15 23:18:00 +0200

    Add light, adjust material

--- public/javascripts/playground.js
+++ public/javascripts/playground.js
@@ -7,16 +7,19 @@ var VIEWPORT = document.getElementById("viewport");
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
+camera.position.z = 5;
 
 var renderer = new THREE.WebGLRenderer({canvas: VIEWPORT});
 renderer.setSize(WIDTH, HEIGHT);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshLambertMaterial({color: 0x0099e6});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+var light = new THREE.PointLight(0xffffff);
+light.position.set(-50,100,100);
+scene.add(light);
 
 function render() {
 	cube.rotation.x += 0.01;
