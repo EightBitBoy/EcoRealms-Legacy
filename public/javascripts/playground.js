47d7411e18fe16504c29a25bbd505e9ef610e995
Stefan Härtel <haertel.dev@gmail.com>
2016-06-15 17:07:35 +0200

    Set up a scene

--- public/javascripts/playground.js
+++ public/javascripts/playground.js
@@ -1,1 +1,18 @@
 console.log("Hello from playground.js!")
+
+var WIDTH = 800
+var HEIGHT = 600
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( WIDTH, HEIGHT );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
