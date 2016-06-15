7552b0d82bf838768850126d43f5850cee638e7c
Stefan Härtel <haertel.dev@gmail.com>
2016-06-15 22:44:47 +0200

    Cleanup

--- public/javascripts/playground.js
+++ public/javascripts/playground.js
@@ -6,21 +6,23 @@ var HEIGHT = 450
 var VIEWPORT = document.getElementById("viewport");
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer({canvas: VIEWPORT});
-renderer.setSize( WIDTH, HEIGHT );
-//document.body.appendChild( renderer.domElement );
+renderer.setSize(WIDTH, HEIGHT);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
 function render() {
-	requestAnimationFrame( render );
-	renderer.render( scene, camera );
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	requestAnimationFrame(render);
+	renderer.render(scene,camera);
 }
+
 render();
