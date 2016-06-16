bacad4c4cfe0a4ae01e7c7e8a3a1dfd30a65d164
Stefan Härtel <haertel.dev@gmail.com>
2016-06-16 18:30:05 +0200

    Add prototype declaration

--- public/javascripts/ebb-grid.js
+++ public/javascripts/ebb-grid.js
@@ -4,6 +4,14 @@ EBB.Grid = function(){
     console.log('Creating a new grid!');
 };
 
+EBB.Grid.prototype = {
+    addLine: function() {
+	},
+
+    foobar: function() {
+    }
+}
+
 EBB.Point = function(x, y){
     this.x = x;
     this.y = y;
