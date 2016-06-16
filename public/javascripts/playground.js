7ccfa13f11c056e20a9d60d79e8909801770a8e5
Stefan Härtel <haertel.dev@gmail.com>
2016-06-16 18:19:17 +0200

    Add a line

--- public/javascripts/playground.js
+++ public/javascripts/playground.js
@@ -9,7 +9,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
 camera.position.z = 5;
 
-var renderer = new THREE.WebGLRenderer({canvas: VIEWPORT});
+var renderer = new THREE.WebGLRenderer({canvas: VIEWPORT, antialias: false});
 renderer.setSize(WIDTH, HEIGHT);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -21,9 +21,20 @@ var light = new THREE.PointLight(0xffffff);
 light.position.set(-50,100,100);
 scene.add(light);
 
+var lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00, linewidth: 10});
+var lineGeometry = new THREE.Geometry();
+lineGeometry.vertices.push(new THREE.Vector3(-2, 0, 0));
+lineGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+lineGeometry.vertices.push(new THREE.Vector3(2, 0, 0));
+var line = new THREE.Line(lineGeometry, lineMaterial);
+scene.add(line)
+
+var grid = new EBB.Grid();
+
 function render() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
 	requestAnimationFrame(render);
 	renderer.render(scene,camera);
 }
