8620d82fabd0f6f1ce926971c907bffccd4a5794
Stefan Härtel <haertel.dev@gmail.com>
2016-06-29 10:11:13 +0200

    Add favicon stuff, does not seem to work yet

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // includes
 var express = require('express');
+var favicon = require('serve-favicon');
 var path = require('path');
 var app = express();
 
 // setup
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
